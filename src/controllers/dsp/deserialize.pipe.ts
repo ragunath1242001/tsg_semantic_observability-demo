1984774a501f3073ea5682a3dfb91718fca80dd9
Maarten Kollenstart <maarten.kollenstart@tno.nl>
2023-08-16 20:27:56 +0200

    Updated deserialization pipe

--- src/controllers/dsp/deserialize.pipe.ts
+++ src/controllers/dsp/deserialize.pipe.ts
@@ -1,9 +1,20 @@
 import { Injectable, PipeTransform } from "@nestjs/common";
 import { deserialize } from "../../model/serialize";
+import { SerializableClass } from "../../model/dsp/common";
+import { ContextDto } from "../../model/dsp/common.dto";
+
+// tslint:disable-next-line: no-any
+type Constructor<T> = new (...args: any[]) => T;
 
 @Injectable()
-export class DeserializePipe<InType, OutType> implements PipeTransform<InType, Promise<OutType>> {
+export class DeserializePipe<InType extends ContextDto, OutType extends SerializableClass<InType>> implements PipeTransform<InType, Promise<OutType>> {
+  constructor(private readonly type?: Constructor<OutType>) {}
   async transform(value: InType): Promise<OutType> {
-    return await deserialize(value);
+    const transformed = await deserialize<OutType>(value);
+    if (this.type === undefined || transformed instanceof this.type) {
+      return transformed;
+    } else {
+      throw Error(`Incorrect deserialization of ${this.type.name}`);
+    }
   }
 }
