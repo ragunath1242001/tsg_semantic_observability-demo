fc2b77bebafc33afcb210181e60d99f26a28a1d7
Maarten Kollenstart <maarten.kollenstart@tno.nl>
2023-09-05 12:09:56 +0200

    Added health controller

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ServerConfig } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ServerConfig);
-  console.log(JSON.stringify(config));
+  Logger.log(`Listening on ${config.listen}:${config.port} with public address ${config.publicAddress}`, 'App')
   await app.listen(config.port, config.listen);
 }
 bootstrap();
