ed45a74b24b89c43e35f042ab75646c40ac9cdc2
Maarten Kollenstart <maarten.kollenstart@tno.nl>
2024-01-08 17:47:30 +0100

    Migrated transfer service to TypeORM

---
+++ src/model/dsp/transfer/transfer.dto.ts
@@ -0,0 +1,88 @@
+import { DataPlaneTransferDto } from "../../data-planes/dataPlanes.dto";
+import { Multilanguage } from "../common";
+import { TransferProcess, DataAddress } from "./messages";
+import { TransferState } from "./messages.dto";
+
+export type TransferRole = "provider" | "consumer";
+
+export interface ITransferEvent {
+  time: Date
+  state: TransferState
+  localMessage?: string
+  code?: string
+  reason?: Multilanguage[]
+  type: "local" | "remote"
+}
+
+export class TransferEvent {
+  time: Date
+  state: TransferState
+  localMessage?: string
+  code?: string
+  reason?: Multilanguage[]
+  type: "local" | "remote"
+
+  constructor(value: ITransferEvent) {
+    this.time = value.time;
+    this.state = value.state;
+    this.localMessage = value.localMessage;
+    this.code = value.code;
+    this.reason = value.reason;
+    this.type = value.type;
+  }
+}
+
+export interface ITransferStatus {
+  localId: string
+  remoteId: string
+  role: TransferRole
+  remoteAddress: string
+  remoteParty: string
+  state: TransferState
+  process: TransferProcess
+  agreementId: string
+  format?: string
+}
+
+export class TransferStatus {
+  localId: string
+  remoteId: string
+  role: TransferRole
+  remoteAddress: string
+  remoteParty: string
+  state: TransferState
+  process: TransferProcess
+  agreementId: string
+  format?: string
+
+  constructor(value: ITransferStatus) {
+    this.localId = value.localId;
+    this.remoteId = value.remoteId;
+    this.role = value.role;
+    this.remoteAddress = value.remoteAddress;
+    this.remoteParty = value.remoteParty;
+    this.state = value.state;
+    this.process = value.process;
+    this.agreementId = value.agreementId;
+    this.format = value.format;
+  }
+}
+
+export interface ITransferDetail extends ITransferStatus {
+  dataAddress?: DataAddress
+  dataPlaneTransfer: DataPlaneTransferDto
+  events: TransferEvent[]
+}
+
+export class TransferDetail extends TransferStatus {
+  dataAddress?: DataAddress
+  dataPlaneTransfer: DataPlaneTransferDto
+  events: TransferEvent[]
+
+  constructor(value: ITransferDetail) {
+    super(value);
+    this.dataAddress = value.dataAddress; 
+    this.dataPlaneTransfer = value.dataPlaneTransfer; 
+    this.events = value.events; 
+  }
+}
