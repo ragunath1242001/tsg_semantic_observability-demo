aec0fe9c65196bb7d1d0ed2a02014fdd0087218f
Maarten Kollenstart <maarten.kollenstart@tno.nl>
2024-04-10 18:11:18 +0200

    feat: update dashboard interface

--- apps/frontend/src/views/Dashboard.vue
+++ apps/frontend/src/views/Dashboard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { axiosInstance } from "../store/index.js";
 import { useToast } from "primevue/usetoast";
 import { DataPlaneStateDto, TransferDto } from "@libs/dtos";
@@ -11,6 +11,20 @@ const toast = useToast();
 const showDataset = ref(false);
 const state = ref<DataPlaneStateDto>();
 const transfers = ref<TransferDto[]>();
+const consumerTransfers = computed(() => {
+  return transfers.value?.filter(t => t.role === "consumer")
+})
+const providerTransfers = computed(() => {
+  return transfers.value?.filter(t => t.role === "provider")
+})
+const showConsumer = computed(() => {
+  return state.value?.details?.role === 'consumer' || state.value?.details?.role === 'both'
+});
+const showProvider = computed(() => {
+  return state.value?.details?.role === 'provider' || state.value?.details?.role === 'both'
+});
+const expandedConsumerRows = ref();
+const expandedProviderRows = ref();
 
 const getState = async () => {
   try {
@@ -40,6 +54,21 @@ const getTransfers = async () => {
   }
 }
 
+const stateSeverity = (state: string) => {
+  switch (state) {
+    case "dspace:STARTED":
+      return "primary";
+    case "dspace:COMPLETED":
+      return "success";
+    case "dspace:REQUESTED":
+      return "info";
+    case "dspace:TERMINATED":
+      return "danger";
+    case "dspace:SUSPENDED":
+      return "warning";
+  }
+}
+
 onMounted(async () => {
   await getState();
   await getTransfers();
@@ -52,49 +81,144 @@ onMounted(async () => {
       <template #title>State</template>
       <template #subtitle>State of this HTTP data plane</template>
       <template #content>
-        <template v-if="state">
-          <FormField label="Identifier">{{ state.identifier }}</FormField>
-          <FormField label="Type">{{ state.details.dataplaneType }}</FormField>
-          <FormField label="Synchronization">{{ state.details.catalogSynchronization }}</FormField>
-          <FormField label="Role">{{ state.details.role }}</FormField>
-          <FormField label="Dataset ID">{{ state.dataset["@id"] }}</FormField>
-
-          <Button
-            label="Show DCAT dataset"
-            @click="showDataset = true"
-          />
-          <Dialog
-            v-model:visible="showDataset"
-            modal
-            header="DCAT dataset"
-            :style="{ width: '90vw', maxWidth: '75rem' }"
-          >
-            <JsonTreeView
-              :data="JSON.stringify(state.dataset)"
-              color-scheme="dark"
-              root-key="Dataset"
-              :max-depth="5"
-            />
-          </Dialog>
-        </template>
+        <div class="grid" v-if="state">
+            <div class="col-12 lg:col-8">
+            <FormField label="Identifier">{{ state.identifier }}</FormField>
+            <FormField label="Type">{{ state.details.dataplaneType }}</FormField>
+            <FormField label="Synchronization">{{ state.details.catalogSynchronization }}</FormField>
+            <FormField label="Role">{{ state.details.role }}</FormField>
+            <FormField label="Dataset ID">{{ state.dataset["@id"] }}</FormField>
+          </div>
+          <div class="col-12 lg:col-4">
+            <div>
+              <Button
+                icon="pi pi-refresh"
+                severity="info"
+                label="Refresh state at Control Plane"
+              />
+            </div>
+            <div class="mt-3">
+              <Button
+                label="Show DCAT dataset"
+                @click="showDataset = true"
+              />
+              <Dialog
+                v-model:visible="showDataset"
+                modal
+                header="DCAT dataset"
+                :style="{ width: '90vw', maxWidth: '75rem' }"
+              >
+                <JsonTreeView
+                  :data="JSON.stringify(state.dataset)"
+                  color-scheme="dark"
+                  root-key="Dataset"
+                  :max-depth="5"
+                />
+              </Dialog>
+            </div>
+          </div>
+        </div>
       </template>
     </Card>
-    <Card class="mt-5">
-      <template #title>Transfers</template>
-      <template #subtitle>Transfers executed by this data plane</template>
+    <Card class="mt-5" v-if="showConsumer">
+      <template #title>Consuming Transfers</template>
+      <template #subtitle>Transfers executed by this data plane acting as consumer</template>
       <template #content>
         <DataTable
-          :value="transfers"
-          sort-field="id"
-          :sort-order="1"
+          v-model:expanded-rows="expandedConsumerRows"
+          :value="consumerTransfers"
+          sort-field="createdDate"
+          :sort-order="-1"
           paginator
           :rows="10"
         >
-          <Column field="id" header="ID" />
-          <Column field="role" header="Role" />
-          <Column field="processId" header="Process ID" />
-          <Column field="state" header="State" />
-          <Column field="createdDate" header="Date" />
+          <Column expander style="width: 5rem" />
+          <Column field="remoteId" header="Remote ID">
+            <template #body="props">
+              did:web:unknown
+            </template>
+          </Column>
+          <Column field="state" header="State">
+            <template #body="props">
+              <Tag :severity="stateSeverity(props.data.state)" :value="props.data.state.replace(/^dspace:/,'')"/>
+            </template>
+          </Column>
+          <Column field="createdDate" header="Date">
+            <template #body="props">
+              {{ new Date(props.data.createdDate).toLocaleString() }}
+            </template>
+          </Column>
+          <Column header="Quick actions">
+            <template #body="props">
+              <Button icon="pi pi-times" :disabled="['dspace:COMPLETED', 'dspace:TERMINATED'].includes(props.data.state)" severity="danger" aria-label="Stop" outlined />
+              <Button v-if="props.data.state === 'dspace:STARTED'" class="ml-2" icon="pi pi-pause" severity="warning" aria-label="Suspend" outlined />
+              <Button v-else :disabled="props.data.state !== 'dspace:SUSPENDED'" class="ml-2" icon="pi pi-play" severity="warning" aria-label="Suspend" outlined />
+              <Button class="ml-2" :disabled="props.data.state !== 'dspace:STARTED'" icon="pi pi-download" severity="info" aria-label="Suspend" outlined />
+              <Button class="ml-2" :disabled="props.data.state !== 'dspace:STARTED'" icon="pi pi-check" severity="success" aria-label="Complete" outlined />
+            </template>
+          </Column>
+          <template #expansion="props">
+            <FormField label="Local ID">{{ props.data.id }}</FormField>
+            <FormField label="Process ID">{{ props.data.processId }}</FormField>
+            <FormField label="Date">{{ new Date(props.data.createdDate).toLocaleString() }}</FormField>
+            <FormField label="State"><Tag :severity="stateSeverity(props.data.state)" :value="props.data.state.replace(/^dspace:/,'')"/></FormField>
+            <FormField label="Agreement">{{ props.data.request['dspace:agreementId'] }}</FormField>
+            <template v-if="props.data.state === 'dspace:STARTED'">
+              <h4>Data address</h4>
+              <FormField label="Endpoint">{{ props.data.dataAddress['dspace:endpoint'] }}</FormField>
+              <FormField label="Properties">
+                <div v-for="property in props.data.dataAddress['dspace:endpointProperties']">
+                  <strong>{{ property['dspace:name'] }}</strong>: {{ property['dspace:value'] }}
+                </div>
+              </FormField>
+            </template>
+          </template>
+        </DataTable>
+      </template>
+    </Card>
+    <Card class="mt-5" v-if="showProvider">
+      <template #title>Providing Transfers</template>
+      <template #subtitle>Transfers executed by this data plane acting as provider</template>
+      <template #content>
+        <DataTable
+          v-model:expanded-rows="expandedProviderRows"
+          :value="providerTransfers"
+          sort-field="createdDate"
+          :sort-order="-1"
+          paginator
+          :rows="10"
+        >
+          <Column expander style="width: 5rem" />
+          <Column field="remoteId" header="Remote ID">
+            <template #body="props">
+              did:web:unknown
+            </template>
+          </Column>
+          <Column field="state" header="State">
+            <template #body="props">
+              <Tag :severity="stateSeverity(props.data.state)" :value="props.data.state.replace(/^dspace:/,'')"/>
+            </template>
+          </Column>
+          <Column field="createdDate" header="Date">
+            <template #body="props">
+              {{ new Date(props.data.createdDate).toLocaleString() }}
+            </template>
+          </Column>
+          <Column header="Quick actions">
+            <template #body="props">
+              <Button icon="pi pi-times" :disabled="['dspace:COMPLETED', 'dspace:TERMINATED'].includes(props.data.state)" severity="danger" aria-label="Stop" outlined />
+              <Button v-if="props.data.state === 'dspace:STARTED'" class="ml-2" icon="pi pi-pause" severity="warning" aria-label="Suspend" outlined />
+              <Button v-else :disabled="!['dspace:SUSPENDED', 'dspace:REQUESTED'].includes(props.data.state)" class="ml-2" icon="pi pi-play" severity="warning" aria-label="Suspend" outlined />
+              <Button class="ml-2" :disabled="props.data.state !== 'dspace:STARTED'" icon="pi pi-check" severity="success" aria-label="Complete" outlined />
+            </template>
+          </Column>
+          <template #expansion="props">
+            <FormField label="Local ID">{{ props.data.id }}</FormField>
+            <FormField label="Process ID">{{ props.data.processId }}</FormField>
+            <FormField label="Date">{{ new Date(props.data.createdDate).toLocaleString() }}</FormField>
+            <FormField label="State"><Tag :severity="stateSeverity(props.data.state)" :value="props.data.state.replace(/^dspace:/,'')"/></FormField>
+            <FormField label="Agreement">{{ props.data.request['dspace:agreementId'] }}</FormField>
+          </template>
         </DataTable>
       </template>
     </Card>
