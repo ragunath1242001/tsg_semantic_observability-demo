f877fbb941c291f18e1f2aa0923aa7bd08992876
Willem Datema <willem.datema@tno.nl>
2024-05-15 11:58:48 +0200

    feat: add config validation for unnecessary parameters

--- apps/backend/src/config.module.ts
+++ apps/backend/src/config.module.ts
@@ -6,6 +6,8 @@ import {
 } from "nest-typed-config";
 import { RootConfig } from "./config";
 import { DynamicModule } from "@nestjs/common";
+import { plainToInstance } from "class-transformer";
+import { validateSync } from "class-validator";
 
 let configModule: DynamicModule;
 let rootConfig: RootConfig;
@@ -26,17 +28,40 @@ try {
       }),
       dotenvLoader({
         separator: "__",
-        keyTransformer: (key) =>
-          key
-            .toLowerCase()
-            .replace(/([a-z]_[a-z])/g, (g) => g[0] + g[2].toUpperCase()),
+        keyTransformer: (key) => {
+          if (key.startsWith("TSGDP__")) {
+            return key
+              .slice(7)
+              .toLowerCase()
+              .replace(/([a-z]_[a-z])/g, (g) => g[0] + g[2].toUpperCase());
+          } else {
+            return "";
+          }
+        },
       }),
     ],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    validate: (rawConfig: any) => {
+      delete rawConfig[""];
+      const config = plainToInstance(RootConfig, rawConfig);
+      const schemaErrors = validateSync(config, {
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        skipMissingProperties: false,
+      });
+
+      if (schemaErrors.length) {
+        throw new Error(TypedConfigModule.getConfigErrorMessage(schemaErrors));
+      }
+      return config as RootConfig;
+    },
   });
   rootConfig = selectConfig(configModule, RootConfig);
 } catch (err) {
   if (err instanceof Error) {
     console.error(err.message);
+    console.log("Waiting 30 seconds before exit");
+    setTimeout(() => {}, 30000);
   } else {
     console.error(err);
   }
