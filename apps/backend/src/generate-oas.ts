b0cfa396483cee5b05d681725be37c8f02dd6cda
Maarten Kollenstart <maarten.kollenstart@tno.nl>
2024-05-21 17:51:11 +0200

    docs: add generic OpenAPI properties

--- apps/backend/src/generate-oas.ts
+++ apps/backend/src/generate-oas.ts
@@ -25,23 +25,41 @@ async function bootstrap() {
   const scopes = Object.fromEntries(appRoles);
   const config = new DocumentBuilder()
     .setTitle("TSG Wallet")
-    .setDescription("Description")
+    .setDescription(
+      `The TSG Wallet is a SSI wallet that can issue and store verifiable credentials and create presentations to be used within data spaces.<br /><br />
+    The wallet is aimed at multi-tier deployments, with one (or more) wallet that acts as trust anchor for a data space and indivual wallets for each of the participants in the data space. On this page the central wallet that issues credentials will be called the Dataspace Wallet although it is not required that there always is exactly one issuer of credentials in a dataspace.<br /><br />
+    __*Note*__: This OpenAPI definition is not intended to be directly linked with a single Wallet instance. `
+    )
+    .setLicense(
+      "Apache 2.0",
+      "https://www.apache.org/licenses/LICENSE-2.0.html"
+    )
+    .setExternalDoc(
+      "Git Repository",
+      "https://gitlab.com/tno-tsg/dataspace-protocol/wallet"
+    )
     .setVersion("1.0")
-    .addTag("Health")
-    .addTag("Settings")
-    .addTag("Authentication")
-    .addTag("DID")
-    .addTag("Contexts")
-    .addTag("Keys")
-    .addTag("Credentials")
-    .addTag("Management DID")
-    .addTag("Management Contexts")
-    .addTag("Management Keys")
-    .addTag("Management Credentials")
-    .addTag("Management Gaia-X Credentials")
-    .addTag("OpenID 4 Verifiable Credential Issuance")
-    .addTag("Presentation IATP")
-    .addTag("Presentation Direct")
+    .addTag("Health", "Health controller")
+    .addTag("Settings", "Settings controller")
+    .addTag("Authentication", "Authentication Controller")
+    .addTag("DID", "DID Controller")
+    .addTag("Contexts", "Contexts Controller")
+    .addTag("Keys", "Keys Controller")
+    .addTag("Credentials", "Credentials Controller")
+    .addTag("Management DID", "Management DID Controller")
+    .addTag("Management Contexts", "Management Contexts Controller")
+    .addTag("Management Keys", "Management Keys Controller")
+    .addTag("Management Credentials", "Management Credentials Controller")
+    .addTag(
+      "Management Gaia-X Credentials",
+      "Management Gaia-X Credentials Controller"
+    )
+    .addTag(
+      "OpenID 4 Verifiable Credential Issuance",
+      "OpenID 4 Verifiable Credential Issuance Controller"
+    )
+    .addTag("Presentation IATP", "Presentation IATP Controller")
+    .addTag("Presentation Direct", "Presentation Direct Controller")
     .addOAuth2({
       type: "oauth2",
       flows: {
