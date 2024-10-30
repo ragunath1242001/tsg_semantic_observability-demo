86f8ef3f9703e58e651d2aa3ef477546bbe627f2
Willem Datema <willem.datema@tno.nl>
2024-10-30 09:19:26 +0000

    fix: remove keys on delete !48

--- apps/wallet-api/src/credentials/credentials.service.ts
+++ apps/wallet-api/src/credentials/credentials.service.ts
@@ -20,7 +20,7 @@ export class CredentialsService {
     @InjectRepository(Credentials)
     private readonly credentialRepository: Repository<Credentials>,
     private readonly didService: DidService,
-    private readonly signatureService: SignatureService
+    private readonly signatureService: SignatureService,
   ) {
     this.initialized = this.init();
   }
@@ -30,14 +30,14 @@ export class CredentialsService {
   async init() {
     this.logger.log("Initializing CredentialService");
     await Promise.all(
-      this.config.initCredentials.map((c) => this.insertIfNotExists(c))
+      this.config.initCredentials.map((c) => this.insertIfNotExists(c)),
     );
     return true;
   }
 
   private async insertIfNotExists(
     initCredentialConfig: InitCredentialConfig,
-    retry = 0
+    retry = 0,
   ): Promise<void> {
     try {
       const existing = await this.credentialRepository.findOneBy({
@@ -45,28 +45,28 @@ export class CredentialsService {
       });
       if (!existing) {
         this.logger.log(
-          `Creating initial credential ${initCredentialConfig.id}`
+          `Creating initial credential ${initCredentialConfig.id}`,
         );
         await this.selfIssueCredential(
           initCredentialConfig,
-          initCredentialConfig.credentialSubject.id
+          initCredentialConfig.credentialSubject.id,
         );
       } else {
         this.logger.log(
-          `Using existing initial credential ${initCredentialConfig.id}`
+          `Using existing initial credential ${initCredentialConfig.id}`,
         );
       }
     } catch (err) {
       if (retry < 5) {
         this.logger.warn(
-          `Retrying creating credential ${initCredentialConfig.id}`
+          `Retrying creating credential ${initCredentialConfig.id}`,
         );
         this.logger.log(`Error: ${err}`);
         await new Promise((f) => setTimeout(f, 10000));
         await this.insertIfNotExists(initCredentialConfig, ++retry);
       } else {
         this.logger.error(
-          `Could not create credential ${initCredentialConfig.id}: ${err}`
+          `Could not create credential ${initCredentialConfig.id}: ${err}`,
         );
         throw err;
       }
@@ -83,7 +83,7 @@ export class CredentialsService {
 
   async getCredential(
     credentialId: string,
-    targetDid?: string
+    targetDid?: string,
   ): Promise<Credentials> {
     const credential = await this.credentialRepository.findOneBy({
       id: credentialId,
@@ -92,7 +92,7 @@ export class CredentialsService {
     if (credential === null) {
       throw new AppError(
         `Credential with identifier ${credentialId} can't be found`,
-        HttpStatus.NOT_FOUND
+        HttpStatus.NOT_FOUND,
       ).andLog(this.logger, "debug");
     }
     return credential;
@@ -100,7 +100,7 @@ export class CredentialsService {
 
   async issueCredential(
     credentialConfig: InitCredentialConfig,
-    targetDid?: string
+    targetDid?: string,
   ): Promise<Credentials> {
     const existing = await this.credentialRepository.findOneBy({
       id: credentialConfig.id,
@@ -108,7 +108,7 @@ export class CredentialsService {
     if (existing) {
       throw new AppError(
         `Credential with identifier ${credentialConfig.id} already exists`,
-        HttpStatus.CONFLICT
+        HttpStatus.CONFLICT,
       ).andLog(this.logger, "debug");
     }
     return await this.selfIssueCredential(credentialConfig, targetDid);
@@ -119,29 +119,29 @@ export class CredentialsService {
       const credentials = await Promise.all(
         this.config.oid4vci.holder.flatMap(async (holder) => {
           const response = await axios.get(
-            `${holder.issuerUrl}/api/credentials`
+            `${holder.issuerUrl}/api/credentials`,
           );
           return response.data;
-        })
+        }),
       ).then((credentialArrays) => [].concat(...credentialArrays));
       return credentials;
     } catch (err) {
       throw new AppError(
         `Could not fetch credentials at dataspace wallet`,
-        HttpStatus.BAD_REQUEST
+        HttpStatus.BAD_REQUEST,
       ).andLog(this.logger);
     }
   }
 
   async importCredential(
     credential: VerifiableCredential<CredentialSubject>,
-    targetDid?: string
+    targetDid?: string,
   ): Promise<Credentials> {
     const didId = targetDid || (await this.didService.getDidId());
     if (!credential.id?.startsWith(`${didId}#`)) {
       throw new AppError(
         "Imported credentials must be have an ID that starts with a DID appended with # and a credential ID",
-        HttpStatus.BAD_REQUEST
+        HttpStatus.BAD_REQUEST,
       ).andLog(this.logger, "warn");
     }
     return await this.credentialRepository.save({
@@ -155,7 +155,7 @@ export class CredentialsService {
   async updateCredential(
     credentialId: string,
     credential: InitCredentialConfig | VerifiableCredential<CredentialSubject>,
-    targetDid?: string
+    targetDid?: string,
   ): Promise<Credentials> {
     await this.getCredential(credentialId, targetDid);
     if (credential instanceof InitCredentialConfig) {
@@ -178,18 +178,18 @@ export class CredentialsService {
     if (credential === null) {
       throw new AppError(
         `Credential with identifier ${credentialId} can't be found`,
-        HttpStatus.NOT_FOUND
+        HttpStatus.NOT_FOUND,
       ).andLog(this.logger, "debug");
     }
-    await this.credentialRepository.softRemove(credential);
+    await this.credentialRepository.remove(credential);
   }
 
   async selfIssueCredential(
     credentialConfig: InitCredentialConfig,
-    targetDid?: string
+    targetDid?: string,
   ): Promise<Credentials> {
     this.logger.log(
-      `Creating verifiable credential for ${credentialConfig.id}`
+      `Creating verifiable credential for ${credentialConfig.id}`,
     );
     const issuanceDate = new Date();
     const expirationDate = new Date();
@@ -213,7 +213,7 @@ export class CredentialsService {
 
     const proof = await this.signatureService.signAsJsonWebSignature(
       credential,
-      credentialConfig.keyId
+      credentialConfig.keyId,
     );
 
     const verifiableCredential: VerifiableCredential<CredentialSubject> = {
@@ -224,8 +224,8 @@ export class CredentialsService {
       `Verifiable credential ${credentialConfig.id}\n${JSON.stringify(
         verifiableCredential,
         null,
-        2
-      )}`
+        2,
+      )}`,
     );
     return await this.credentialRepository.save({
       id: credentialId,
