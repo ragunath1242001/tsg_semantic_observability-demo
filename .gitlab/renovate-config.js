89c9dd3abb542218f56bdf7ffe8e5b7d28288abe
Maarten Kollenstart <maarten.kollenstart@tno.nl>
2024-11-27 15:12:06 +0000

    ci: add initial renovate config !70

---
+++ .gitlab/renovate-config.js
@@ -0,0 +1,10 @@
+export default {
+  platform: "gitlab",
+  endpoint: process.env.CI_API_V4_URL,
+  token: process.env.RENOVATE_TOKEN,
+  onboarding: false,
+  onboardingConfig: {
+    extends: ["config:recommended"]
+  },
+  repositories: [process.env.CI_PROJECT_PATH]
+};
